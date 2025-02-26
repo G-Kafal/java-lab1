37722cac4c23b5d0f865eb87838032ff84801c1a
G-Kafal <grivenkafalinji51@gmail.com>
2025-02-26 10:27:48 +0100

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,9 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("Hello Vistula");
-        System.out.println("Hello Vistula");
+        System.out.println("Welcome to Vistula  University ,based in Warsaw,Poland");
+        System.out.println("Hi,My name is Griven");
 
     }
+
 }
